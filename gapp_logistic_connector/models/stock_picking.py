d7d577666ac07513a68e7cdf991906960ed714ec
jobiols <jorge.obiols@gmail.com>
2019-12-11 11:21:18 -0300

    [WIP] ajustes para que el archivo de intercambio contemple lotes

--- gapp_logistic_connector/models/stock_picking.py
+++ gapp_logistic_connector/models/stock_picking.py
@@ -61,118 +61,143 @@ class StockPicking(models.Model):
     def encode_data_file(self):
         """ Codificar archivo para GAPP
         """
+        import wdb;wdb.set_trace()
+
         datas = b''
+        # chequear que cada linea tiene al menos un lote
         for line in self.move_lines:
-            cols = list()
+            if not line.move_line_ids:
+                raise UserWarning('No hay lotes definidos para el '
+                                  'producto %s' % line.product_id.name)
 
-            # 1. Código del propietario del LOTE/ARCHIVO.
-            cols.append('RANDOM')
+        for line in self.move_lines:
+            for lot in line.move_line_ids:
+                cols = list()
 
-            # 2. Letra del comprobante que respalda el pedido. Ej:”A”
-            origin = self.origin
-            sale_order_obj = self.env['sale.order']
-            so = sale_order_obj.search([('name', '=', origin)])
-            if not so:
-                raise exceptions.UserError(_('No se puede encontrar la orden '
-                                             'de venta relacionada con este '
-                                             'envio.'))
-            if not so.invoice_ids[0]:
-                raise exceptions.UserError(_('No se puede encontrar la '
-                                             'factura de venta que respalda '
-                                             'este envio.'))
-            letter = so.invoice_ids[0].document_letter_name
-            cols.append('{}'.format(letter))
+                # 1. Código del propietario del LOTE/ARCHIVO.
+                cols.append('RANDOM')
 
-            # 3. Centro Emisor (Uso Interno) Siempre = “001”
-            cols.append('001')
+                # 2. Letra del comprobante que respalda el pedido. Ej:”A”
+                origin = self.origin
+                sale_order_obj = self.env['sale.order']
+                so = sale_order_obj.search([('name', '=', origin)])
+                if not so:
+                    raise exceptions.UserError(_('No se puede encontrar la orden '
+                                                 'de venta relacionada con este '
+                                                 'envio.'))
 
-            # 4. Numero de pedido. El numero que representa al pedido registro.
-            cols.append('{:010}'.format(self.id))
+                if not (so.invoice_ids and so.invoice_ids[0]):
+                    raise exceptions.UserError(_('No se puede encontrar la '
+                                                 'factura de venta que respalda '
+                                                 'este envio.'))
 
-            # 5. Código del cliente/sucursal a la que pertenece el pedido.
-            ref = self.partner_id.ref
-            try:
-                ref = int(ref)
-            except:
-                raise exceptions.UserError(_('La referencia del cliente debe '
-                                             'ser un numero'))
-            cols.append('{:010}'.format(ref))
+                letter = so.invoice_ids[0].document_letter_name
+                cols.append('{}'.format(letter))
 
-            # 6. Es la fecha propuesta de entrega al cliente.
-            dt = self.scheduled_date
-            if dt:
-                cols.append('{}'.format(dt.strftime('%d/%m/%Y')))
-            else:
-                cols.append('')
+                # 3. Centro Emisor (Uso Interno) Siempre = “001”
+                cols.append('001')
 
-            # 7. Nombre o razón social asociado al campo 5, que representa al
-            # destinatario del pedido.
-            name = self.partner_id.name
-            cols.append('{}'.format(name))
+                # 4. Numero de pedido. El numero que representa al pedido registro.
+                cols.append('{:010}'.format(self.id))
 
-            # 8. Código postal asociado a la dirección del destinatario.
-            zipcode = self.partner_id.zip
-            if zip:
+                # 5. Código del cliente/sucursal a la que pertenece el pedido.
+                ref = self.partner_id.ref
+                try:
+                    ref = int(ref)
+                except:
+                    raise exceptions.UserError(_('La referencia del cliente debe '
+                                                 'ser un numero'))
+                cols.append('{:010}'.format(ref))
+
+                # 6. Es la fecha propuesta de entrega al cliente.
+                dt = self.scheduled_date
+                if dt:
+                    cols.append('{}'.format(dt.strftime('%d/%m/%Y')))
+                else:
+                    cols.append('')
+
+                # 7. Nombre o razón social asociado al campo 5, que representa al
+                # destinatario del pedido.
+                name = self.partner_id.name
+                cols.append('{}'.format(name))
+
+                # 8. Código postal asociado a la dirección del destinatario.
+                zipcode = self.partner_id.zip or ''
                 cols.append('{}'.format(zipcode))
-            else:
-                cols.append('')
 
-            # 9. Nombre de la calle y altura asociado a la dirección del
-            # destinatario.
-            street = self.partner_id.street
-            cols.append('{}'.format(street))
+                # 9. Nombre de la calle y altura asociado a la dirección del
+                # destinatario.
+                street = self.partner_id.street or ''
+                cols.append('{}'.format(street))
 
-            # 10. Nombre de la localidad asociada a la dirección del
-            # destinatario.
-            city = self.partner_id.street2 or ''
-            city += ' ' + self.partner_id.state_id.name or ''
-            cols.append('{}'.format(city.strip()))
+                # 10. Nombre de la localidad asociada a la dirección del
+                # destinatario.
+                city = self.partner_id.street2 or ''
+                city += ' ' + (self.partner_id.state_id.name or '')
+                cols.append('{}'.format(city.strip()))
 
-            # 11. Código de provincia asociada a la dirección del destinatario.
-            provincia = self.encode_state(self.partner_id.state_id.id or False)
-            if provincia:
-                cols.append('{}'.format(provincia))
-            else:
-                cols.append('')
+                # 11. Código de provincia asociada a la dirección del destinatario.
+                provincia = self.encode_state(self.partner_id.state_id.id or False)
+                if provincia:
+                    cols.append('{}'.format(provincia))
+                else:
+                    cols.append('')
 
-            # 12. Peso asociado a la cantidad de productos de la línea actual
-            # del archivo. Ej: Si se solicitan 10 unidades  del producto
-            # 000001, que el peso individual es 2.5 kilos, el valor
-            # será = 25.000 (veinticinco con 3 ceros decimales).
-            peso = self.weight
-            if peso:
-                cols.append('{0:0.3f}'.format(peso))
-            else:
-                cols.append('')
+                # 12. Peso asociado a la cantidad de productos de la línea actual
+                # del archivo. Ej: Si se solicitan 10 unidades  del producto
+                # 000001, que el peso individual es 2.5 kilos, el valor
+                # será = 25.000 (veinticinco con 3 ceros decimales).
+                peso = lot.product_id.weight * lot.product_uom_qty
+                if peso:
+                    cols.append('{0:0.3f}'.format(peso))
+                else:
+                    cols.append('')
 
-            # 13. Una observación asociada al pedido.
-            obs = self.note
-            cols.append('{}'.format(obs))
+                # 13. Una observación asociada al pedido.
+                obs = self.note or ''
+                cols.append('{}'.format(obs))
 
-            # 14. Código del producto solicitado.
-            default_code = line.product_id.default_code
-            cols.append('{}'.format(default_code))
+                # 14. Código del producto solicitado.
+                default_code = lot.product_id.default_code
+                if default_code:
+                    cols.append('{}'.format(default_code))
+                else:
+                    raise UserWarning('El producto %s no tiene '
+                                      'codigo' % lot.product_id.name)
 
-            # 15. Cantidad solicitada del producto en el registro vigente.
-            qty = line.product_qty
-            cols.append('{:010.3f}'.format(qty))
+                # 15. Cantidad solicitada del producto en el registro vigente.
+                qty = lot.product_uom_qty
+                cols.append('{:010.3f}'.format(qty))
 
-            # 16. Lote particular al cual debe pertenecer el producto
-            # solicitado.
-            lot = ''
-            cols.append('{}'.format(lot))
+                # 16. Lote particular al cual debe pertenecer el producto
+                # solicitado.
 
-            # 17. Código seriado del producto solicitado.
-            serial = ''
-            cols.append('{}'.format(serial))
+                lot_name = lot.lot_id.name
+                cols.append('{}'.format(lot_name))
 
-            # 18. Monto, Subtotal o importe que representa la cantidad
-            # solicitada del producto en el registro. (Sin incluir IVA).
-            monto = ''
-            cols.append('{}'.format(monto))
+                # 17. Código seriado del producto solicitado. NO SE USA
+                serial = ''
+                cols.append('{}'.format(serial))
 
-            line_data = ';'.join(cols)
+                # 18. Monto, Subtotal o importe que representa la cantidad
+                # solicitada del producto en el registro. (Sin incluir IVA).
 
-            datas += b'%s\r\n' % bytes(line_data, encoding='utf8')
+                # para hacer esto voy a la factura que representa este
+                # movimiento y busco el precio unitario de este producto luego
+                # lo multiplico por la cantidad de este lote
+                monto = False
+                invoice = so.invoice_ids[0]
+                for invoice_line in invoice.invoice_line_ids:
+                    if invoice_line.product_id.id == lot.product_id.id:
+                        monto = lot.product_uom_qty * invoice_line.price_unit
+                if monto:
+                    cols.append('{:010.3f}'.format(monto))
+                else:
+                    cols.append('')
+
+                line_data = ';'.join(cols)
+
+                datas += b'%s\r\n' % bytes(line_data, encoding='utf8')
+
 
         return datas
